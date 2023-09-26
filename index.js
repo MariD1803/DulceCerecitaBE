e13280aed64c0f3b2958cfdd96c1eff7371f7fd4
Your Name <mariangeldiazv2011@gmail.com>
2023-09-26 02:42:17 -0300

    update vercel 0

---
+++ index.js
@@ -0,0 +1,29 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const persona_routes_1 = __importDefault(require("./src/routes/persona.routes"));
+const mensaje_routes_1 = __importDefault(require("./src/routes/mensaje.routes"));
+const orden_routes_1 = __importDefault(require("./src/routes/orden.routes"));
+const producto_routes_1 = __importDefault(require("./src/routes/producto.routes"));
+const usuario_routes_1 = __importDefault(require("./src/routes/usuario.routes"));
+const status_routes_1 = __importDefault(require("./src/routes/status.routes"));
+const app = (0, express_1.default)();
+var allowCrossDomain = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+};
+app.use(express_1.default.json());
+app.use(allowCrossDomain);
+app.use('/api', persona_routes_1.default);
+app.use('/api', mensaje_routes_1.default);
+app.use('/api', orden_routes_1.default);
+app.use('/api', producto_routes_1.default);
+app.use('/api', usuario_routes_1.default);
+app.use('/api', status_routes_1.default);
+app.listen(3000);
+console.log("Server on port", 3000);
